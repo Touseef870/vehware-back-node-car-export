2c68f782dc476acf070bb74325a0c484f279fa09
sharjeel hussain <sharjeelhussain877@gmail.com>
2025-01-31 22:15:00 +0500

    remove two items

--- src/product/models/index.js
+++ src/product/models/index.js
@@ -6,19 +6,19 @@ const CarSchema = new Schema({
         required: true,
         trim: true
     },
-    sellerEmail: {
-        type: String,
-        required: [true, 'Email is required'],
-        trim: true,
-        match: [
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            'Please enter a valid email address'
-        ]
-    },
-    sellerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, "Pleaes provide seller id"]
-    },
+    // sellerEmail: {
+    //     type: String,
+    //     required: [true, 'Email is required'],
+    //     trim: true,
+    //     match: [
+    //         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    //         'Please enter a valid email address'
+    //     ]
+    // },
+    // sellerId: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     required: [true, "Pleaes provide seller id"]
+    // },
     // price: {
     //     type: Number,
     //     required: true,
